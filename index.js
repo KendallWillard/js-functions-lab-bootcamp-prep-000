8cd94e93aa192f4c8709f1ed4caedbe43237781a
kendall willard <kendallwillard@hotmail.com>
2019-01-27 23:15:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  var result = "It\'s " + days + " until " + holiday + "!"
+  var result = "It\'s " + holiday + " until " + days + "!"
   return result
 }
